bfb49c8d696628c9235c4d0d70c14ff63d52bcee
Kamarthi-Bhoomika <kbhoomika05@gmail.com>
2023-09-05 15:32:42 +0530

    Web API commit

---
+++ Data/MovieManagementContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApiManagement.Models;
+
+namespace WebApiManagement.Data
+{
+    public class MovieManagementContext : DbContext
+    {
+        public MovieManagementContext (DbContextOptions<MovieManagementContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApiManagement.Models.Movie> Movie { get; set; } = default!;
+    }
+}
